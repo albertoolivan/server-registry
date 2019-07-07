4480e77b33c6bab823b744d86f17ffda556907c6
Alberto <Alberto@Alberto-PC>
2019-07-07 11:59:53 +0200

    @test fixed

--- src/test/java/org/adidas/code/challange/server/registry/AppTest.java
+++ src/test/java/org/adidas/code/challange/server/registry/AppTest.java
@@ -1,38 +1,32 @@
 package org.adidas.code.challange.server.registry;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.assertj.core.api.BDDAssertions.then;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@Test
+	public void shouldStartEurekaServer() {
+		ResponseEntity<String> entity = this.testRestTemplate
+				.getForEntity("http://localhost:" + this.port + "/eureka/apps", String.class);
+
+		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
